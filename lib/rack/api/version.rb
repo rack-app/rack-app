f5c2dc32c88da4f7c1ed36ac2bab2ec991fc8347
Adam Luzsi <adamluzsi@gmail.com>
2015-10-03 23:05:40 +0200

    fix old namespace leftover in version.

--- lib/rack/api/version.rb
+++ lib/rack/api/version.rb
@@ -1,3 +1,3 @@
 Rack ||= Module.new
-Rack::App ||= Class.new
-Rack::App::VERSION = File.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'VERSION')).strip
+Rack::API ||= Class.new
+Rack::API::VERSION = File.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'VERSION')).strip
