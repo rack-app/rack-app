2fee212deb24602e28de7bce7a59bb814d837348
Adam Luzsi <adamluzsi@gmail.com>
2015-10-04 17:52:01 +0200

    rack app utils for generic helper logics

---
+++ lib/rack/app/utils.rb
@@ -0,0 +1,19 @@
+module Rack::APP::Utils
+  extend self
+  # Normalizes URI path.
+  #
+  # Strips off trailing slash and ensures there is a leading slash.
+  #
+  #   normalize_path("/foo")  # => "/foo"
+  #   normalize_path("/foo/") # => "/foo"
+  #   normalize_path("foo")   # => "/foo"
+  #   normalize_path("")      # => "/"
+  def normalize_path(path)
+    path = "/#{path}"
+    path.squeeze!('/')
+    path.sub!(%r{/+\Z}, '')
+    path = '/' if path == ''
+    path
+  end
+
+end
---
+++ spec/rack/utils_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe Rack::APP::Utils do
+
+  def new_subject
+    Object.new.tap { |o| o.extend(described_class) }
+  end
+
+  describe '#normalize_path' do
+    subject { new_subject.normalize_path(request_path) }
+
+    context 'when path is as how expected' do
+      let(:request_path) { '/foo' }
+
+      it { is_expected.to eq '/foo' }
+    end
+
+    context 'when per given even in the end' do
+      let(:request_path) { '/baz/' }
+
+      it { is_expected.to eq '/baz' }
+    end
+
+    context 'when no per given in the path' do
+      let(:request_path) { 'bar' }
+
+      it { is_expected.to eq '/bar' }
+    end
+
+    context 'when empty string given' do
+      let(:request_path) { '' }
+
+      it { is_expected.to eq '/' }
+    end
+
+  end
+
+end
