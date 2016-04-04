95fd61e5e1ec14a9290434359a998c353e2ee380
Adam Luzsi <adamluzsi@gmail.com>
2016-04-04 14:43:11 +0200

    test: add some extra test to validate not overriding behavior

--- lib/rack/app/middlewares/header_setter.rb
+++ lib/rack/app/middlewares/header_setter.rb
@@ -12,17 +12,10 @@ class Rack::App::Middlewares::HeaderSetter
       response.headers[header] ||= value
     end
 
-    set_content_type(response)
-
     response.finish
   end
 
   protected
-
-  def set_content_type(response)
-    return unless @headers[::Rack::CONTENT_TYPE]
-    response.headers[::Rack::CONTENT_TYPE]= @headers[::Rack::CONTENT_TYPE]
-  end
 
   def rack_response(raw_response)
     Rack::Response.new(raw_response[2], raw_response[0], raw_response[1])
--- spec/rack/app/middlewares/header_setter_spec.rb
+++ spec/rack/app/middlewares/header_setter_spec.rb
@@ -15,11 +15,17 @@ describe Rack::App do
         '{}'
       end
 
+      get '/acao' do
+        response.headers['Access-Control-Allow-Origin']= 'not default'
+      end
+
     end
 
     it { expect(get(:url => '/').headers[::Rack::CONTENT_TYPE]).to eq 'application/json' }
 
     it { expect(get(:url => '/').headers['Access-Control-Allow-Origin']).to eq '*' }
+
+    it { expect(get(:url => '/acao').headers['Access-Control-Allow-Origin']).to eq 'not default' }
 
     it { expect(get(:url => '/').headers['Access-Control-Expose-Headers']).to eq 'X-My-Custom-Header, X-Another-Custom-Header' }
 
