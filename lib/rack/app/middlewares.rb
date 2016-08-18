0c6e81fe172f01c402ca9cfb7607f25bfdac17da
Adam Luzsi <adamluzsi@gmail.com>
2016-08-18 12:03:33 +0200

    fix: left behind the require block of the removed middleware

--- lib/rack/app/middlewares.rb
+++ lib/rack/app/middlewares.rb
@@ -1,7 +1,6 @@
 module Rack::App::Middlewares
-  require 'rack/app/middlewares/header_setter'
-  require 'rack/app/middlewares/method_override'
-  require 'rack/app/middlewares/params'
   require 'rack/app/middlewares/configuration'
+  require 'rack/app/middlewares/header_setter'
+  require 'rack/app/middlewares/params'
   require 'rack/app/middlewares/hooks'
 end
