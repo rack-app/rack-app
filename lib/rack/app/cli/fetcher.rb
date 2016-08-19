6c7770d6a43272f538db243102960b560b994fe9
Adam Luzsi <adamluzsi@gmail.com>
2016-08-19 20:00:56 +0200

    fix: endpoint duplication because server fetcher loaded config.ru multiple times

--- lib/rack/app/cli/fetcher.rb
+++ lib/rack/app/cli/fetcher.rb
@@ -12,22 +12,39 @@ module Rack::App::CLI::Fetcher
   end
 
   def rack_app
+    server_based_lookup || rack_app_with_most_endpoints
+  end
 
-    server = Rack::App::CLI::Fetcher::Server.new(:config => 'config.ru')
+  protected
 
-    app = server.app
+  def server_based_lookup
+    @server_based_lookup ||= lambda do
+      server = Rack::App::CLI::Fetcher::Server.new(:config => 'config.ru')
+      app = server.app
+      last_app = nil
+      until app.is_a?(Class) && app <= Rack::App
+        raise if app.__id__ == last_app.__id__
 
-    until app.is_a?(Class) && app <= Rack::App
-      app.instance_variables.each do |ivar|
-        value = app.instance_variable_get(ivar)
-        if value.respond_to?(:call) and not [Method, Proc, UnboundMethod].include?(value.class)
-          app = value
+        app.instance_variables.each do |ivar|
+          value = app.instance_variable_get(ivar)
+          if value.respond_to?(:call) and not [Method, Proc, UnboundMethod].include?(value.class)
+            app = value
+          end
         end
+
+        last_app = app
       end
-    end
+      app
+    end.call 
+  rescue
+  end
 
-    app
-
+  def rack_app_with_most_endpoints
+    ObjectSpace.each_object(Class).select{|klass|
+      klass < Rack::App
+    }.uniq.sort_by{ |rack_app|
+      rack_app.router.endpoints.length
+    }.last
   end
 
 end
