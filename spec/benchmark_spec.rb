533f0e9eb06933907bdb3d55f3a9ee4a8e98dbce
Adam Luzsi <adamluzsi@gmail.com>
2016-08-30 21:57:56 +0200

    test: add test for edge case, large number of endpoint decleration speed

--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -4,6 +4,7 @@ require File.join(File.dirname(__FILE__), 'benchmark_spec', 'rack_skeleton')
 require 'spec_helper'
 require 'benchmark'
 require 'rack/app/test'
+require 'timeout'
 
 describe '#Performance Benchmark' do
 
@@ -33,6 +34,46 @@ describe '#Performance Benchmark' do
       let(:request_path) { '/users/123' }
 
       it { is_expected.to be < maximum_accepted_seconds }
+    end
+
+  end
+
+  describe 'route tree generation time' do
+    include Rack::App::Test
+    context 'when only static endpoints given' do
+      rack_app Class.new(Rack::App)
+
+      it 'should initialize the application in a meaningful time' do
+        Timeout.timeout(10) do
+          time_now = Time.now
+          rack_app do
+            10_000.times do |i|
+              get "/#{i}" do
+                'Hello, World!'
+              end
+            end
+          end
+          puts(Time.now - time_now) if ENV['VERBOSE'] =~ /^t/i
+        end
+      end
+    end
+
+    context 'when only dynamic endpoints given' do
+      rack_app Class.new(Rack::App)
+
+      it 'should initialize the application in a meaningful time' do
+        Timeout.timeout(10) do
+          time_now = Time.now
+          rack_app do
+            10_000.times do |i|
+              get "/#{i}/:id" do
+                'Hello, World!'
+              end
+            end
+          end
+          puts(Time.now - time_now) if ENV['VERBOSE'] =~ /^t/i
+        end
+      end
     end
 
   end
