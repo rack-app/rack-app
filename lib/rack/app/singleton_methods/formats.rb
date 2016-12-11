d4ac6d1c726173055e2e6eb691f28cb2d45a4121
Adam Luzsi <adamluzsi@gmail.com>
2016-12-11 02:42:26 +0100

    refactor: change code style in SingletonMethods/Formats

--- lib/rack/app/singleton_methods/formats.rb
+++ lib/rack/app/singleton_methods/formats.rb
@@ -1,17 +1,15 @@
 module Rack::App::SingletonMethods::Formats
-
   def formats(&descriptor)
     @formats_builder ||= Rack::App::Serializer::FormatsBuilder.new
     unless descriptor.nil?
       @formats_builder.instance_exec(&descriptor)
-      router.reset 
+      router.reset
     end
     @formats_builder
   end
 
-  def serializer(default_content_type=nil, &how_to_serialize)
-    formats{ default(default_content_type,&how_to_serialize) } unless how_to_serialize.nil?
-    return formats.to_serializer
+  def serializer(default_content_type = nil, &how_to_serialize)
+    formats { default(default_content_type, &how_to_serialize) } unless how_to_serialize.nil?
+    formats.to_serializer
   end
-
 end
