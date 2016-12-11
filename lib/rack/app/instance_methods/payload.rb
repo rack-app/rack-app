7ed620f29d661953bf27d90d14d2376fc7ade031
Adam Luzsi <adamluzsi@gmail.com>
2016-12-11 14:02:53 +0100

    revert: remove parsing from payload_stream method

--- lib/rack/app/instance_methods/payload.rb
+++ lib/rack/app/instance_methods/payload.rb
@@ -2,12 +2,10 @@ module Rack::App::InstanceMethods::Payload
 
   def payload_stream(&block)
     return nil unless @request.body.respond_to?(:gets)
-    parser = request.env[Rack::App::Constants::ENV::PAYLOAD_PARSER]
-    content_type = request.content_type
-    while chunk = request.body.gets
-      block.call(parser.parse_string(content_type, chunk))
+    while chunk = @request.body.gets
+      block.call(chunk)
     end
-    request.body.rewind
+    @request.body.rewind
     nil
   end
 
@@ -18,9 +16,7 @@ module Rack::App::InstanceMethods::Payload
   def payload_to_file(file_path, file_mod='w')
     return nil unless @request.body.respond_to?(:gets)
     File.open(file_path, file_mod) do |file|
-      while chunk = request.body.gets
-        file.print(chunk)
-      end
+      payload_stream{ |chunk| file.print(chunk) }
     end
   end
 
