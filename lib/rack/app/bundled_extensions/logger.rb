e9d2504b3b0d6e7d69e9a4c4ae89f484e6e04269
Adam Luzsi <adamluzsi@gmail.com>
2016-12-14 12:04:58 +0100

    refactor: minor code refactoring in logger ext

--- lib/rack/app/bundled_extensions/logger.rb
+++ lib/rack/app/bundled_extensions/logger.rb
@@ -5,11 +5,12 @@ Rack::App::Extension.register(:logger) do
 
   define_method(:logger) do
     @logger ||= lambda do
+      id = SecureRandom.hex
       log = ::Logger.new(STDOUT)
       original_formatter = Logger::Formatter.new
       log.formatter = proc do |severity, datetime, progname, msg|
         fmsg = msg.is_a?(::Hash) ? (defined?(::JSON) ? JSON.dump(msg) : msg) : msg
-        fprname = progname || request.env['HTTP_X_REQUEST_ID'] || SecureRandom.hex
+        fprname = progname || request.env['HTTP_X_REQUEST_ID'] || id
         original_formatter.call(severity, datetime, fprname, fmsg)
       end
       return log
