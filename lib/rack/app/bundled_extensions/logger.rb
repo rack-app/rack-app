423d3e3ee09c538fd377f79ee4fc6c89fe19aa1a
Adam Luzsi <adamluzsi@gmail.com>
2016-12-14 12:02:05 +0100

    refactor: clean up logger extension code

--- lib/rack/app/bundled_extensions/logger.rb
+++ lib/rack/app/bundled_extensions/logger.rb
@@ -3,51 +3,17 @@ Rack::App::Extension.register(:logger) do
   require "logger"
   require "securerandom"
 
-  logger_class = Class.new
-  logger_class.class_eval do
-
-    def initialize(request_id)
-      @request_id = request_id
-      @logger = ::Logger.new(STDOUT)
-    end
-
-    [:info, :warn, :error, :fatal, :unknown].each do |log_level|
-      define_method(log_level) do |msg|
-        if msg.is_a?(Hash)
-          @logger.__send__(log_level, @request_id){format_hash(msg)}
-        else
-          @logger.__send__(log_level, @request_id){String(msg)}
-        end
-      end
-    end
-
-    protected
-
-    begin
-      require "json"
-    rescue LoadError
-    end
-
-    def format_hash(hash)
-      if defined?(::JSON)
-        JSON.dump(hash)
-      else
-        hash.inspect
-      end
-    end
-
-    def method_missing(name,*args)
-      if @logger.respond_to?(:binding)
-        @logger.__send__(name,*args)
-      else
-        super
-      end
-    end
-
-  end
-
   define_method(:logger) do
-    @logger ||= logger_class.new(request.env['HTTP_X_REQUEST_ID'] || SecureRandom.hex)
+    @logger ||= lambda do
+      log = ::Logger.new(STDOUT)
+      original_formatter = Logger::Formatter.new
+      log.formatter = proc do |severity, datetime, progname, msg|
+        fmsg = msg.is_a?(::Hash) ? (defined?(::JSON) ? JSON.dump(msg) : msg) : msg
+        fprname = progname || request.env['HTTP_X_REQUEST_ID'] || SecureRandom.hex
+        original_formatter.call(severity, datetime, fprname, fmsg)
+      end
+      return log
+    end.call
   end
 
 end
