74515c74bab88230401a8a1ab5e7a048ca374a76
Adam Luzsi <adamluzsi@gmail.com>
2017-04-12 08:28:40 +0200

    chore: format with rubocop the new hook spec files

--- spec/rack/app/hooks/after_spec.rb
+++ spec/rack/app/hooks/after_spec.rb
@@ -1,19 +1,18 @@
-require "spec_helper"
+require 'spec_helper'
 describe Rack::App do
   include Rack::App::Test
 
   describe '.after' do
     rack_app do
-
       def self.queue
         @queue ||= []
       end
 
       after { self.class.queue << 'after' }
       after do
-        if params["break_in_after"]
+        if params['break_in_after']
           resp = Rack::Response.new
-          resp.write("breaked")
+          resp.write('breaked')
           respond_with(resp)
         end
       end
@@ -23,15 +22,13 @@ describe Rack::App do
 
         'OK'
       end
-
     end
 
     it 'should be executed after the endpoint definition' do
-       expect(get('/').body).to eq 'OK'
-       expect(rack_app.queue).to eq %w(in after)
+      expect(get('/').body).to eq 'OK'
+      expect(rack_app.queue).to eq %w[in after]
     end
 
-    it { expect(get('/', :params => {"break_in_after" => true}).body).to eq 'breaked' }
+    it { expect(get('/', params: { 'break_in_after' => true }).body).to eq 'breaked' }
   end
-
 end
--- spec/rack/app/hooks/before_spec.rb
+++ spec/rack/app/hooks/before_spec.rb
@@ -1,24 +1,20 @@
-require "spec_helper"
+require 'spec_helper'
 describe Rack::App do
   include Rack::App::Test
 
   describe '.before' do
     rack_app do
-
-      before { respond_with("breaked") if params["break_in_before"] }
+      before { respond_with('breaked') if params['break_in_before'] }
       before { @say = 'Hello' }
       before { @word = 'World' }
 
       get '/say' do
         "#{@say}, #{@word}!"
       end
-
     end
 
     it { expect(get('/say').body).to eq 'Hello, World!' }
 
-    it { expect(get('/say', :params => {"break_in_before" => true}).body).to eq 'breaked' }
-
+    it { expect(get('/say', params: { 'break_in_before' => true }).body).to eq 'breaked' }
   end
-
 end
