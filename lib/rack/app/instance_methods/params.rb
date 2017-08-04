67ed0fb51c789c3d2dfab59130b57ea3f0368fad
Adam Luzsi <adamluzsi@gmail.com>
2017-08-05 01:56:05 +0200

    remove deprecated method

--- lib/rack/app/instance_methods/params.rb
+++ lib/rack/app/instance_methods/params.rb
@@ -6,12 +6,6 @@ module Rack::App::InstanceMethods::Params
     request.env[E::PARAMS].to_hash
   end
 
-  def validated_params
-    request.env[E::PARAMS].validated_params
-  end
-
-  Rack::App::Utils.deprecate(self, :validated_params, :params, 2017, 07)
-
   def path_segments_params
     request.env[E::PARAMS].path_segments_params
   end
